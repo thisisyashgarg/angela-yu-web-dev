104ed7d7f7cc5e2d875c7bb7e05587233772dc67
Yash Garg <thisisyashgarg@gmail.com>
2022-11-26 13:17:13 +0530

    used axios instead of fetch, corrected formatting, changed view of weather api

--- section-20/newsletterApp/api.js
+++ section-20/newsletterApp/api.js
@@ -8,8 +8,8 @@ import client from "@mailchimp/mailchimp_marketing";
 
 
 const app = express();
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); // for images and css to run on node
+app.use(bodyParser.urlencoded({extended: true})); // for getting req body
 
 app.get('/', (req, res) =>{
  res.sendFile(__dirname + '/signup.html');
@@ -29,28 +29,27 @@ app.post('/', (req, res) =>{
     
     const run = async () => {
         try{
-                const response = await client.lists.batchListMembers(process.env.listId, {
-                    members: [
-                      {
-                          "email_address": email,
+            const response = await client.lists.batchListMembers(process.env.listId, {
+                members: [
+                    {
+                        "email_address": email,
                           "status": 'subscribed',
                           "merge_fields": {
-                              "FNAME": firstName,
-                              "LNAME": lastName,
+                            "FNAME": firstName,
+                            "LNAME": lastName,
                           }
-                      }
-                            ],
-                  });
-                  
-                  console.log(response);
-            }
+                    }
+                ],
+            });
+            console.log(response);
+        }
 
         catch{
-                res.send('error 404')
-            }
+            res.send('error 404');
+        }
     };
     run();
-    res.send('no error, subscribed');  
+    res.send('subscribed');  
 });   
 
 app.listen(process.env.PORT || 3000 , () => console.log('3000'));
