5cc71e75b4169441fd6bee3a02ade8d55a0a48ed
Yash Garg <thisisyashgarg@gmail.com>
2022-12-02 21:24:53 +0530

    completely built the rest api

--- section-31/app.js
+++ section-31/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config();
+import ejs from 'ejs'
 import path from 'path';
 const __dirname = path.resolve();
 import bodyParser from 'body-parser'
@@ -19,29 +20,84 @@ const articleSchema = {
 }
 const Article = new mongoose.model("Article", articleSchema);
 
-
-//routing
-app.get('/articles', (req, res) =>{
-   Article.find((err, articles) =>{
-    if(err){
-        res.send(err);
-    }
-    res.send(articles);
-   })
-});
-
-app.post('/articles', (req, res) =>{
-    console.log(req.body.title);
-    console.log(req.body.content);
-
+//////////////////////////////////////////////////////ARTICLES
+app.route('/articles')
+.get((req, res) =>{
+    Article.find((err, articles) =>{
+     if(err){
+         res.send(err);
+     }
+     res.send(articles);
+    })
+ })
+.post((req, res) =>{
     const newArticle = new Article({
         title: req.body.title,
         content : req.body.content
     });
-    newArticle.save((data) => console.log(`${data} ADDED`));
-    res.send('data added')
-
+    newArticle.save((err) => {
+        if(!err){
+            console.log(`DATA ADDED`);
+            res.send('data added');
+        }
+    });
+ })
+.delete((req, res) =>{
+    Article.deleteMany(function (err) {
+        if(!err){
+            res.send('Succesfully deleted all articles')
+        }
+        res.send(err);
+    })
  });
+ 
+
+//////////////////////////////////////////////////////ARTICLES/YASHGARG
+
+app.route('/articles/:articleTitle')
+.get((req, res) =>{
+    Article.findOne({title: req.params.articleTitle}, (err, foundArticle) => {
+        if(err){
+        res.send("No articles matching the given title");
+        }
+        res.send(foundArticle);
+    })
+})
+.put((req, res) => {
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {override: true},
+        (err) =>{
+           if(!err){
+            res.send("article updated successfully")
+           }
+           res.send(err)
+        }
+    )
+})
+.patch((req, res) => {
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        (err) =>{
+           if(!err){
+            res.send("article updated successfully")
+           }
+           res.send(err)
+        }
+    );
+})
+.delete((req, res) =>{
+    Article.deleteOne({title: req.params.articleTitle}, function (err) {
+        if(!err){
+            res.send('Succesfully deleted the article')
+        }
+        res.send(err);
+    })
+});
+
+
 
 app.listen(5002 , () => console.log('listening on 5002'));
 
